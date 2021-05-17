f2720626c7948f5b10685771e70e7ffaf258d387
LuisGomez2106 <lugomezr@unal.edu.co>
2021-05-16 23:45:00 -0500

    lunes 17 añadido

--- src/pages/Semana1.jsx
+++ src/pages/Semana1.jsx
@@ -21,7 +21,46 @@ const Semana1 = center => (
     <Header title="Semana 17-23 de Mayo">Actividades que puedes hacer esta semana </Header>
     <Container center={center}>
      <h3>
-     Lunes 17 
+     Lunes 17
+      <P>
+      <p> <a href="https://www.instagram.com/p/CO9aY96tp9G/?igshid=1aex4lb6vdo8c" target="_blank"> Consulte información de Medellin aqui </a></p>
+       <p>
+     8am Olla Comunitaria - Parque San Antonio de Prado(Medellin)
+     </p>
+     <p>
+     10:30am Movilizacion - Glorieta el Hatillo(Medellin)
+     </p>
+     <p>
+     11am Retorno al Paro - Estación Niquira/Exito Robledo/Estación La Estrella(Medellin)
+     </p>
+     <p>
+     12pm Asamblea Popular - Parque la Resistencia(Medellin)
+     </p>
+      <p>
+     1:30pm Movilización Mujeres del Norte - Glorieta Estación Niquira(Medellin)
+     </p>
+      <p>
+     2pm Caravana motera bloqueo Palmas - Primer Mirador de las Palmas(Medellin)
+     </p>
+       <p>
+     2pm Todxs lxs maricas,lesbianas,trans resistimos - Museo Casa de la Memoria(Medellin)
+     </p>
+       <p>
+     3pm Velatón cultural contra la violencia a la población LGBTI - Calle Barbacoas CLL 57a con Crr 46(Medellin)
+     </p>
+       <p>
+     3pm Vamos a Pararte - Barnaby Jones bar(Envigado)
+     </p>
+       <p>
+     3pm Arte al servicio de la Revolución - Manga Biblioteca Débora Arango(Envigado)
+     </p>
+       <p>
+     5pm Plantón por la vida - Parque San Javier(Medellin)
+     </p>
+       <p>
+     7pm Velatón en honor a Allison - Parque Principal de Bello(Medellin)
+     </p>
+     </P>
      </h3>
      <h3>
      Martes 18 
